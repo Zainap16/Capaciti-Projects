48104669dde0959805e988328664db294499fde2
Zainap16 <122858207+Zainap16@users.noreply.github.com>
2023-04-24 15:51:17 +0200

    day 1 work

---
+++ JavaScript Intermediate/Week 3/Day 1/index.js
@@ -0,0 +1,81 @@
+
+// console.log(global.luckyNum);
+
+// global.luckyNum='23';
+
+// console.log(global.luckyNum);
+//ACT 1
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req, res) => {
+// res.statusCode = 200;
+// res.setHeader('Content-Type', 'text/plain');
+// res.end("Hello World\n");
+// });
+// server.listen(port, hostname, () => {
+// console.log('Server running at http://${hostname}:${port}/');
+// });
+
+//ACT 2 
+
+// var http = require('http'); 
+
+// http.createServer(function (req, res) {
+
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+
+//     res.end('Hello World!');
+
+// }).listen(8080);
+
+
+/* 
+ACT 3
+What is Node JS?
+
+--Node JS is an open source server enviroment
+
+How is Node JS initiated on a computer?
+
+--First download the Node JS. Use terminal/command prompt window, and type 'node' so you can use node. 
+
+Why do we use Node JS?
+
+--Fast and scalable.Node.js uses a non-blocking, event-driven I/O model that allows it to handle multiple requests simultaneously without blocking the execution of other code.  
+
+What can Node JS do?
+
+--Web applicantaion, Games , APIs.
+
+What is a module in Node JS the same as in JavaScript?
+
+--JavaScript: modules refer to pieces of code that can be exported from one file and imported into another file 
+--Node JS: modules are also used to organize code into smaller, reusable units, but they work slightly differently. 
+
+What is NPM?( Node Package Manager)
+
+-- It is used to install, manage, and share packages (also called modules) that are written in JavaScript and can be used in Node.js applications.
+
+What is contained  in a Node JS Package?
+
+--Modules: A Node.js package typically includes one or more modules, which are individual files that contain JavaScript code. Modules can define functions, classes, objects, and other types of code that can be imported and used by other modules.
+--package.json file: This is a JSON file that provides metadata about the package, such as its name, version, description, author, license, and dependencies. It also includes information about scripts that can be run as part of the package, such as build or test scripts.
+
+*/
+
+/*
+What were the important features of the day?
+all
+
+What activities were beneficial?
+all
+
+What activities (if any) were too easy?
+none
+
+What activities need more time?
+
+all
+
+*/

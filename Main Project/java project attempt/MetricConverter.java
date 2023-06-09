a336be342585bb4a8ce367c09a321685406dfc9d
Zainap16 <122858207+Zainap16@users.noreply.github.com>
2023-06-09 07:15:50 +0000

    completed console java metric converter

--- Main Project/java project attempt/MetricConverter.java
+++ Main Project/java project attempt/MetricConverter.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class MetricConverter {
+
     public static void main(String[] args) {
 
         // a. The unit you want to convert from
@@ -11,15 +12,6 @@ public class MetricConverter {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("enter unit you wanna convert from");
-        String ConvertUnitFrom = input.nextLine();
-
-        // System.out.println("enter unit you wanna convert to");
-        // String ConvertUnitTo = input.nextLine();
-
-        System.out.println("enter quantity");
-        double Quantity = input.nextDouble();
-
         // a. From Feet to Meters
 
         // b. From Pounds to Kgs
@@ -28,13 +20,32 @@ public class MetricConverter {
 
         double total = 0;
         final double METERS = 3.281;
-        // final double KGS = 2.205;
+        final double KGS = 2.205;
 
-        if (ConvertUnitFrom.equals("feet")) {
+        System.out.println("enter unit you wanna convert from feet-f,pounds-p,F : ");
+        String ConvertUnitFrom = input.nextLine();
+
+        System.out.println("enter quantity");
+        double Quantity = input.nextDouble();
+        // input.nextLine();
+
+        System.out.println("enter unit you wanna convert to inches-i,kilograms-kg,, C");
+        String ConvertUnitTo = input.nextLine();
+
+        if (ConvertUnitFrom.equals("f") && ConvertUnitTo.equals("i")) {
             total = Quantity / METERS;
-            System.out.println(total);
+            System.out.println(Math.ceil(total));
 
-        } else {
+        } else if (ConvertUnitFrom.equals("p") && ConvertUnitTo.equals("kg")) {
+            total = Quantity / KGS;
+            System.out.println(Math.ceil(total));
+
+        } else if (ConvertUnitFrom.equals("F") && ConvertUnitTo.equals("C")) {
+            total = (Quantity - 32) * (5 / 9);
+            System.out.println(Math.ceil(total));
+        }
+
+        else {
             System.out.println("FAILURE");
         }
 
